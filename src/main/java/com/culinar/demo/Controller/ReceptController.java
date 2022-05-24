6ffc878e5106590c4a877943f4d90ba2190f94f5
Smolth <105016428+Smolth@users.noreply.github.com>
2022-05-24 15:21:08 +0300

    Update ReceptController.java

--- src/main/java/com/culinar/demo/Controller/ReceptController.java
+++ src/main/java/com/culinar/demo/Controller/ReceptController.java
@@ -1,14 +1,17 @@
 package com.culinar.demo.Controller;
 
 import com.culinar.demo.ReceptDAO;
+import com.culinar.demo.model.Ingredient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.websocket.server.PathParam;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class ReceptController {
@@ -16,7 +19,7 @@ public class ReceptController {
     private ReceptDAO receptDAO;
 
 
-    @GetMapping()
+    @GetMapping("/allRecipes")
         public String index (Model model) throws SQLException {
         model.addAttribute("recipes", receptDAO.index());
         return "output/recepts";
@@ -25,5 +28,15 @@ public class ReceptController {
     public String show (@PathVariable ("id") int id, Model model) {
         model.addAttribute("recept", receptDAO.show(id));
         return "output/recept";
+    }
+
+    @PostMapping()
+    public String submit(HttpServletRequest request, Model model,
+                         @RequestParam(name="ingredients", required = false) String[] ingredients){
+        ingredients = request.getParameterValues("ingredients");
+        model.addAttribute("recipes", receptDAO.searchByIngredients(ingredients));
+        if (receptDAO.isFlag()) {
+            return "output/recepts";
+        } else return "output/recipes";
     }
 }
