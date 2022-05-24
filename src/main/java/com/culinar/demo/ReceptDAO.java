5f70373ab954881dd30006fc3987cf5125e34101
Smolth <105016428+Smolth@users.noreply.github.com>
2022-05-24 15:15:47 +0300

    Update ReceptDAO.java

--- src/main/java/com/culinar/demo/ReceptDAO.java
+++ src/main/java/com/culinar/demo/ReceptDAO.java
@@ -86,3 +86,106 @@ public class ReceptDAO {
         return recept;
     }
 }
+
+public List<Recept> searchByIngredients(String[] ingredients) {
+        this.flag = false;
+        String x = "";
+        List<Recept> findingRecipes = new ArrayList<>();
+        List<Recept> similarRecipes = new ArrayList<>();
+        ArrayList<Integer> idOfRecipes = new ArrayList<>();
+        ArrayList<Integer> idOfSimilarRecipes = new ArrayList<>();
+        ArrayList<String> userIngredients;
+        int id;
+
+        try {
+            ArrayList<String> products = new ArrayList<>();
+            PreparedStatement preparedStatement = connection.prepareStatement("" +
+                    "Select id, ingredients " +
+                    "from recept");
+
+            ResultSet rs = preparedStatement.executeQuery();
+
+            while (rs.next()) {
+                id = rs.getInt("id");
+                Array dataIngredients = rs.getArray("ingredients");
+                String[] str_ingredients = (String[]) dataIngredients.getArray();
+                for (String i : str_ingredients) {
+                    products.add(i);
+                }
+
+                userIngredients = new ArrayList<>();
+                for (String i : ingredients) {
+                    userIngredients.add(i);
+                }
+                /*PreparedStatement preparedStatement = connection.prepareStatement("" +
+                        "Select * " +
+                        "from   recept " +
+                        "where  ingredients " +
+                        "similar to '%?%';");*/
+
+                /*for (int i = 0; i < ingredients.length; i++) {
+                    products.add(ingredients[i]);
+                }
+
+                Log log = LogFactory.getLog(this.getClass());
+                String x = "";
+                log.info(x);
+                 for(String i : ingredients){
+                    x = x.concat(i).concat(",");
+                 }*/
+                    for (int j = 0; j < products.size(); j++) {
+                        //int counter = 0;
+
+                        for (int k = 0; k < userIngredients.size(); k++) {
+                            String s = userIngredients.get(k);
+                            if (products.get(j).contains(s)) {
+                                products.remove(j);
+                                userIngredients.remove(k);
+                                k--;
+                                j--;
+                                break;
+                            }else if (products.get(j).contains("по желанию")){
+                                products.remove(j);
+                                j--;
+                                break;
+                            }
+                        }
+                        if (products.isEmpty()) {
+                            idOfRecipes.add(id);
+                            this.flag = true;
+                            break;
+                        }
+                    }
+                    if ((products.size() < 5) & (!this.flag)) {
+                            idOfSimilarRecipes.add(id);
+                            for(String i : products){
+                                x = x.concat(i).concat("; ");
+                            }
+                            String message = "Необходимо добавить следующие ингредиенты: " + x;
+                            PreparedStatement ps = connection.prepareStatement("" +
+                                    "UPDATE recept " +
+                                    "SET adding = ? " +
+                                    "WHERE id = ?;");
+
+                            ps.setString(1, message);
+                            ps.setInt(2, id);
+                            ps.executeUpdate();
+                    }
+                }
+                if (this.flag) {
+                    for (int number : idOfRecipes) {
+                        findingRecipes.add(this.show(number));
+                    }
+                    return findingRecipes;
+                } else {
+                    for (int number : idOfSimilarRecipes) {
+                        similarRecipes.add(this.show(number));
+                    }
+                    return similarRecipes;
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
