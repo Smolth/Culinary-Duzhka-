412231a00073f374d3bc7bb896032caf103ceef5
Smolth <105016428+Smolth@users.noreply.github.com>
2022-05-30 10:32:01 +0300

    ended release

--- src/main/java/com/culinar/demo/Controller/GreetingController.java
+++ src/main/java/com/culinar/demo/Controller/GreetingController.java
@@ -4,9 +4,7 @@ import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class GreetingController implements ErrorController {
@@ -21,9 +19,5 @@ public class GreetingController implements ErrorController {
     @GetMapping("/")
     public String greeting() {
             return "greeting";
-    }
-    @GetMapping("/recept")
-    public String recept() {
-        return "output/recept";
     }
 }
